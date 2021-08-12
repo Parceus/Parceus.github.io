833158718a2401306d64714c05845c0584657391
Parceus <patricklingahan0509@gmail.com>
2021-08-12 15:10:24 +0800

    Adding ILAW proj(outdated)

---
+++ ILAW/inventory.js
@@ -0,0 +1,26 @@
+/*This is for the Burger Menu Button*/ 
+function toggleSidebar(){
+    document.getElementById("sidebar").classList.toggle('active')
+}
+
+/*This is for the Full Screen Button*/ 
+var elem = document.documentElement;
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+}
